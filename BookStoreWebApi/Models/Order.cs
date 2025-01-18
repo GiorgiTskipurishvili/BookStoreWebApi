98ac3161180cd9d63d8d2677c8b214e73d325547
GiorgiTskipurishvili <giorgitskipurishvili@gmail.com>
2025-01-18 17:21:21 +0400

    Models Commit

---
+++ BookStoreWebApi/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStoreWebApi.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public decimal TotalAmount { get; set; }
+        public bool IsConfirmed { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+    }
+}
